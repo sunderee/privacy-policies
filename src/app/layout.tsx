9d7dec76160d5761c6a0f6680551e794e83dda87
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2024-11-27 14:27:53 +0100

    Prepare the home screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -6,15 +7,18 @@ export const metadata: Metadata = {
   description: "Collection of privacy policies and other legalese for projects developed under Peter Aleksander Bizjak s.p.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
