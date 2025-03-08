21b42fb1fedad5de16e2f475c5b8570e6f3d3740
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-03-08 08:49:14 +0100

    Remove Geist fonts and delete Diskurs privacy policy markdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { RootLayoutProps } from "@/types/ui";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Privacy Policies",
@@ -22,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
